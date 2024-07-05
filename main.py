c86a9888c0f37d8b4d701df8ed5321ec30100fdc
3garfield5 <matveykakozyrev@gmail.com>
2024-07-05 12:10:26 +0300

    правки

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from sweater import app, db
 
-# with app.app_context():
-#     db.drop_all()  # очистка БД
-#     db.create_all()  # создание новой таблицы БД
+with app.app_context():
+    db.drop_all()  # очистка БД
+    db.create_all()  # создание новой таблицы БД
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
